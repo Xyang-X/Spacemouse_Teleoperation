bdd3ad36677a8d34924bc7cdd38511928088be11
Xyang-X <11811019@mail.sustech.edu.cn>
2025-09-25 18:56:19 +0800

    add local revision

--- franka_ctl.py
+++ franka_ctl.py
@@ -8,37 +8,30 @@ class franka_spm():
     def __init__(self,translation_inter_scale=0.3,rotation_inter_scale=0.8):
         self.robot = Robot("172.16.0.2")
         # self.gripper = Gripper("172.16.0.2")
-        self.cb1()  # Initialize the robot state
+        self.cb()  # Initialize the robot state
         self.robot.relative_dynamics_factor = RelativeDynamicsFactor(0.05, 0.1, 0.15)
-        joint_state = self.robot.current_joint_state
-        joint_pos = joint_state.position
+        
         self.translation_inter_scale=translation_inter_scale
         self.rotation_inter_scale=rotation_inter_scale
         eular=R.from_quat(self.ee_pose.quaternion).as_euler('xyz', degrees=True)        
         # print(eular)
         print(self.ee_pose)
-        print(f"Initial joint positions: {joint_pos}")
+
         
     def velocity_control(self, twist):
         """
         twist: [vx, vy, vz, wx, wy, wz]
         """
-        self.cb1()
+        self.cb()
         # Transform the velocity to the robot's coordinate system
         linear_velocity= twist[:3]
         angular_velocity= twist[3:]
         m_cv=CartesianVelocityMotion(Twist(linear_velocity, angular_velocity), relative_dynamics_factor=0.2)
-        # m_cv.register_callback(self.cb)  # Register the callback function to be called periodically
-        # self.state = self.robot.state
-        # self.cartesian_state = self.robot.current_cartesian_state
-        # self.robot_pose = self.cartesian_state.pose  # Contains end-effector pose and elbow position
-        # self.ee_pose = self.robot_pose.end_effector_pose
-        # print(twist)
-        # print(self.ee_pose)
+
         self.robot.move(m_cv, asynchronous=True)    
 
 
-    def cb1(self):
+    def cb(self):
         """
         Callback function to be called periodically
         """
@@ -49,13 +42,10 @@ class franka_spm():
         robot_velocity = self.cartesian_state.velocity  # Contains end-effector twist and elbow velocity
         self.ee_twist = robot_velocity.end_effector_twist
         self.elbow_vel = robot_velocity.elbow_velocity
+        joint_state = self.robot.current_joint_state
+        self.joint_pos = joint_state.position
+        self.joint_vel = joint_state.velocity
         
-        # print(self.ee_pose)   
-
-    def cb(robot_state: RobotState, time_step: Duration, rel_time: Duration, abs_time: Duration, control_signal: JointPositions):
-        print(f"At time {abs_time}, the target joint positions were {control_signal.q}")
- 
- 
     def pose_control(self, target_pose,mode=0):
         """
         Move the robot to the target position
@@ -105,9 +95,6 @@ class franka_spm():
         
 if __name__ == "__main__":
     franka_spm_instance = franka_spm()
-    print(franka_spm_instance.ee_pose.translation)
-    franka_spm_instance.pose_control(np.array([0.45,0,0.1,np.pi,0,0]))
-
 
     # franka_spm_instance.velocity_control(np.array([0, 0.1, 0.0, 0.0, 0.0, 0.0], dtype=np.float32))  # Example twist input
     # time.sleep(1)  # Allow some time for the motion to execute
